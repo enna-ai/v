25ac8a8b0c4269e3bf9d87f018faf05d40542929
enna-ai <annelee3322@gmail.com>
2024-03-13 14:59:05 -0600

    ✨ add base files

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+export const SITE_AUTHOR = "Vi";
+export const SITE_DESC = "My portfolio website";
+export const SITE_URL = "https://";
+export const THEME_COLOR = "#C4A7E7";
+export const GITHUB_URL = "https://github.com/enna-ai";
